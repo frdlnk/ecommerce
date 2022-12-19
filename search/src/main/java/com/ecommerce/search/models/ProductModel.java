2b7f76bc0a80e82acdf249205206228287537481
Freddy <nglimaai01@gmail.com>
2022-12-19 11:29:38 -0600

    Models used in rest controller

---
+++ search/src/main/java/com/ecommerce/search/models/ProductModel.java
@@ -0,0 +1,22 @@
+package com.ecommerce.search.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "products")
+public class ProductModel {
+    @Getter @Setter
+    private String _id;
+    @Getter @Setter
+    private String title;
+    @Getter @Setter
+    private String description;
+    @Getter @Setter
+    private Double price;
+    @Getter @Setter
+    private List<String> categories;
+
+}
